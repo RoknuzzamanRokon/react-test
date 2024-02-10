71c951b66ccba4f8f7a9518137d0cb40c693e5a0
iibrahim70 <aviknuhash70@gmail.com>
2024-02-10 20:04:34 +0600

    responsive navbar

--- wealth-free-life-client/src/layout/Navbar.jsx
+++ wealth-free-life-client/src/layout/Navbar.jsx
@@ -1,12 +1,16 @@
-import { Button } from "@/components/ui/button";
-import useToast from "@/hooks/useToast";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "@/providers/AuthProvider";
-import { useContext } from "react";
+import useToast from "@/hooks/useToast";
+import { IoMenu, IoClose } from "react-icons/io5";
 import { Link } from "react-router-dom";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 
 const Navbar = () => {
   const { user, logOut } = useContext(AuthContext);
   const { showToast } = useToast();
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const handleLogout = () => {
     logOut()
@@ -18,25 +22,81 @@ const Navbar = () => {
       });
   };
 
+  const navItems = (
+    <>
+      <Link to="/">Home</Link>
+      <Link to="/trading-bot">Trading Bot</Link>
+      <Link to="/market">Market</Link>
+      <Link to="/blogs">Blogs</Link>
+      <Link to="/contact">Contact Us</Link>
+      {user && (
+        <Button onClick={handleLogout} variant="link" size="link">
+          Signout
+        </Button>
+      )}
+    </>
+  );
+
+  useEffect(() => {
+    if (isMenuOpen) {
+      document.body.classList.add("no-scroll");
+    } else {
+      document.body.classList.remove("no-scroll");
+    }
+  }, [isMenuOpen]);
+
   return (
     <nav className="section-wrapper py-3 flex items-center justify-between">
-      <div>
+      {/* left side */}
+      <div className="flex items-center gap-3">
+        {/* opening button */}
+        <button onClick={() => setIsMenuOpen(true)} className="md:hidden">
+          <IoMenu className="size-6" />
+        </button>
+
+        {/* nav title */}
         <h4>Wealth Free Life</h4>
+
+        {/* mobile navigation overlay */}
+        <div
+          onClick={() => setIsMenuOpen(false)}
+          className={cn(
+            "fixed top-0 left-0 w-full h-full bg-slate-300 bg-opacity-70 backdrop-blur-sm md:hidden",
+            isMenuOpen ? "opacity-100 visible" : "opacity-0 invisible"
+          )}
+        ></div>
+
+        {/* mobile navigation content */}
+        <div
+          className={cn(
+            "absolute top-0 left-0 w-1/2 h-screen md:hidden bg-white px-3 py-10 rounded transition-transform ease-in-out duration-300 z-50",
+            isMenuOpen ? "translate-x-0" : "-translate-x-full"
+          )}
+        >
+          {/* closing button */}
+          <div
+            onClick={() => setIsMenuOpen(false)}
+            className="flex justify-end mb-5"
+          >
+            <button>
+              <IoClose className="size-6" />
+            </button>
+          </div>
+
+          {/* menu items */}
+          <div
+            onClick={() => setIsMenuOpen(false)}
+            className="flex flex-col gap-y-2 items-start"
+          >
+            {navItems}
+          </div>
+        </div>
       </div>
 
-      <div className="flex items-center gap-5">
-        <Link to="/">Home</Link>
-        <Link to="/">Trading Bot</Link>
-        <Link to="/">Market</Link>
-        <Link to="/">Blogs</Link>
-        <Link to="/">Contact Us</Link>
-        {user && (
-          <Button onClick={handleLogout} variant="link" size="link">
-            Signout
-          </Button>
-        )}
-      </div>
+      {/* middle  */}
+      <div className="xs:hidden lg:flex items-center gap-5">{navItems}</div>
 
+      {/* right side */}
       {user ? (
         <Link to="/wallet">
           <Button className="rounded-full">Wallet</Button>
